b8724aa1ee2ddba87844513150f4d68af461bc2f
JooHo Lee, Jay <ljhiyh@gmail.com>
2014-06-19 16:24:22 +0900

    Update Dockfile and script

--- apache/docker-env.sh
+++ apache/docker-env.sh
@@ -1,5 +1,5 @@
 export HOST_NAME="Apache1"
 export IP="172.17.42.20"
-export IMAGE='docker-registry:5000/centos65:apache'
+export IMAGE='docker-registry:5000/centos65:httpd'
 export DNS='172.17.42.100'
 
