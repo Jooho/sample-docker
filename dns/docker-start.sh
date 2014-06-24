0653c34730aa4c98ad1905bd8f2e2e4f8abd68bc
JooHo Lee, Jay <ljhiyh@gmail.com>
2014-06-24 16:20:01 +0900

    change -n option to --net because docker upgrade

--- dns/docker-start.sh
+++ dns/docker-start.sh
@@ -40,12 +40,13 @@ done
 docker run \
 --name="$HOST_NAME" \
 -i -t -h="$HOST_NAME" \
+-d \
 -P \
--n=false \
--lxc-conf="lxc.network.type=veth" \
--lxc-conf="lxc.network.ipv4=${IP}/24" \
--lxc-conf="lxc.network.ipv4.gateway=172.17.42.1" \
--lxc-conf="lxc.network.link=docker0" \
--lxc-conf="lxc.network.name=eth0" \
--lxc-conf="lxc.network.flags=up" \
+--net=none  \
+--lxc-conf="lxc.network.type=veth" \
+--lxc-conf="lxc.network.ipv4=${IP}/24" \
+--lxc-conf="lxc.network.ipv4.gateway=172.17.42.1" \
+--lxc-conf="lxc.network.link=docker0" \
+--lxc-conf="lxc.network.name=eth0" \
+--lxc-conf="lxc.network.flags=up" \
 $IMAGE  $CMD
