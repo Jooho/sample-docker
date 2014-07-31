b08b8faf7b9f73f69127ab97c36f715c3a583c71
Jooho Lee <ljhiyh@gmail.com>
2014-07-31 16:33:06 +0900

    delete tar files

--- mysql/docker-env.sh
+++ mysql/docker-env.sh
@@ -1,6 +1,6 @@
 export HOST_NAME="mysql1"
-export IP="172.17.42.40"
-export IMAGE='docker-registry:5000/rhel65-base:mysql'
+export IP="172.17.42.41"
+export IMAGE='ljhiyh/centos65:mysql'
 #export DNS='192.168.43.220'
 #export DNS='10.64.160.190'
 export DNS='172.17.42.100'
